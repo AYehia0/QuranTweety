8c6bb43c17979911250b309422f7faa6087628af
AYehia0 <ahmedyehia611@gmail.com>
2021-06-29 15:10:34 +0200

    more error handling

--- hadiths.py
+++ hadiths.py
@@ -19,15 +19,28 @@ def request_hadith():
     res = requests.request("GET", url, data=payload, headers=headers)
     res_json = json.loads(res.text)
 
+    # Here i assume no errors happen, only receiving a valid response,, gonna change it later.
+    try :  
+        if res_json["message"] == "Too Many Requests":
+            return None
+    except Exception as e:
+        #No errors
+        pass
+
     return res_json
 
 def format_hadith(res):
     '''Return a formatted hadith from the response'''
- 
+
+    # Checking for errors 
+    if res is None:
+        return None 
+
     # The ar in index: 1
     hadith_body_ar = res['hadith'][1]["body"].replace("<p>", "")
     formatted_hadith =  hadith_body_ar[:len(hadith_body_ar)-4]
 
     return formatted_hadith.lstrip("-")
+
 
 
