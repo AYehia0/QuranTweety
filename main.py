20bc02275d7e1cdef0f942f524d9eac46fa4fbe4
AYehia0 <ahmedyehia611@gmail.com>
2021-09-11 22:08:48 +0200

    added the new feature

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import time
 
 TWEET_SIZE = 280
 AYAHAT_SIZE = 6236
+
 # time in mins : runs every 3h
 TIME = 180
 
@@ -60,7 +61,8 @@ def ensure_get(search_key):
         if search_key == 3:
 
             # getting poem
-            res = get_random_lines(5)
+            #res = get_random_lines(5)
+            res = generate_poem()
 
             if res:
                 if check_size(str(res)):
