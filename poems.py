a51ff92217ca4b35e86af62d00a1d5275e075fd7
AYehia0 <ahmedyehia611@gmail.com>
2021-09-11 22:07:54 +0200

    added auto-generating poems using makrov chains

--- poems.py
+++ poems.py
@@ -1,16 +1,25 @@
 import os
 import time
 import random
-
+import rhymes
+import markovify
 
 poems_location = "poems"
+qafiaa = {
+    'antara' : ['ام', 'ار', 'اها', 'ادي'],
+    'alakhtal': ['دا'],
+    'mutanabi': ['اقا'],
+    'qabani': ['ايا'],
+    'tho-ramla': ['الا']
+}
+
 poem_names = {
         "alakhtal": "الأخطل" ,
         "mutanabi": "المتنبي" ,
         "antara": "عنترة" , 
         "qabani": "قباني" ,
         "tho-ramla": "ذو الرمة"
-    }
+}
 
 def open_file(file_name):
 
@@ -33,7 +42,6 @@ def get_random_lines(line_range=10):
 
     poem_lines = []
 
-
     # choosing random file
     file_name = f"{poems_location}/{get_random_file()}"
     
@@ -51,7 +59,6 @@ def get_random_lines(line_range=10):
     i = 0 
 
     try:
-
         while len(poem_lines) != line_range+1:
 
             current_line = lines[random_line + i]
@@ -67,4 +74,46 @@ def get_random_lines(line_range=10):
         return None
 
     return "\n".join(poem_lines)
+
+def markov(text_file):
+    f = open(text_file, 'r')       
+    text = f.read()
+
+    text_model = markovify.NewlineText(text)
+    return text_model
+
+def generate_poem(iterations=3000):
+
+
+    # getting random file 
+    rand_file = get_random_file()
+
+    author_name = rand_file.split('.')[0]
+    input_file = f"{poems_location}/{rand_file}"
+
+    # getting random rhyme
+    rhyme = random.choice(qafiaa[author_name])
+
+    n_of_rhyme_letters = len(rhyme)
+
+    text_model = markov(input_file)
+    rhymes_list = rhymes.rhymes_with_last_n_chars(rhyme, n_of_rhyme_letters)
+
+    bayts = set()
+    used_rhymes = set()
+    
+    poem = ""
+        
+    for _ in range(iterations):
+        bayt = text_model.make_short_sentence(280, tries=100)
+        last_word = bayt.split()[-1]
+
+        if (last_word in rhymes_list) and (last_word not in used_rhymes) and (bayt not in bayts):
+            bayts.add(bayt)
+            used_rhymes.add(last_word)
+            poem += f"{bayt}\n"
+
+    # adding the author to the tweet
+    poem += f"*{poem_names[author_name]}*"
+    return poem
 
---
+++ rhymes.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+# author: Haydara  https://www.youtube.com/haydara
+
+import pickle
+
+with open('vocabs.pkl', 'rb') as pickle_load:
+    voc_list = pickle.load(pickle_load)
+    
+
+allowed_chars = ['ذ', 'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج', 'د',
+                 'ش', 'س', 'ي', 'ب', 'ل', 'ا', 'أ', 'ت', 'ن', 'م', 'ك', 'ط', 'ئ', 'ء', 'ؤ', 'ر', 'ى', 
+                 'ة', 'و', 'ز', 'ظ', 'ّ', ' ']
+
+max_word_length = 9
+
+def rhymes_with(word):
+
+    if word not in ['الله', 'والله', 'بالله', 'لله', 'تالله']:
+        word = word.replace('ّ', '')
+
+    ending = word[-2:]
+    rhymes = []
+    for w in voc_list:
+        if len(w) < max_word_length and w.endswith(ending):
+            rhymes.append(w)
+    return rhymes
+
+def rhymes_with_last_n_chars(word, n):
+
+    if word not in ['الله', 'والله', 'بالله', 'لله', 'تالله', 'فالله']:
+        word = word.replace('ّ', '')
+    ending = word[-n:]
+    rhymes = []
+
+    for word in voc_list:
+        if len(word) < max_word_length and word.endswith(ending):
+            rhymes.append(word)
+    return rhymes
